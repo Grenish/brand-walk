d40de75fa5baa357674f951c7be782d6a9034ce8
Grenish rai <ariestheracer@gmail.com>
2023-08-12 12:22:23 +0530

    import/export components

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Footer from "./Footer";
+import Kids from "./Kids";
+import Main from "./Main";
+import Mens from "./Mens";
+import Navbar from "./Navbar";
+import Womens from "./Womens";
+import Reg from "./Reg";
+
+export{
+    Footer,
+    Kids,
+    Main,
+    Mens,
+    Navbar,
+    Womens,
+    Reg
+}
