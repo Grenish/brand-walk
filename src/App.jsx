c664c9928db671f86744e8e0596fa170214aae84
Grenish rai <ariestheracer@gmail.com>
2023-08-12 12:20:35 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Navbar, Footer, Mens, Womens, Main, Kids, Reg } from "./components";
 
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} exact />
+        <Route path="/mens" element={<Mens />} />
+        <Route path="/womens" element={<Womens />} />
+        <Route path="/kids" element={<Kids />} />
+        <Route path="/register" element={<Reg />} />
+      </Routes>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
