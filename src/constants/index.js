8b1a4d97b3e92b280bc322e1ab654ffa3ce7f9a6
Grenish rai <ariestheracer@gmail.com>
2023-08-12 12:23:51 +0530

    constants update

---
+++ src/constants/index.js
@@ -0,0 +1,106 @@
+import { aditi, sahil, rohan, priyanka } from "../assets";
+
+import {
+  AdidasShoe1,
+  AdidasShoe2,
+  AdidasShoe3,
+  AdidasShoe4,
+  AdidasShoe5,
+  AdidasShoe6,
+  AdidasShoe7,
+  AdidasShoe8,
+  AdidasShoe9,
+  AdidasShoe10,
+  AdidasShoe11,
+  ConverseShoe1,
+  ConverseShoe2,
+  ConverseShoe3,
+  ConverseShoe4,
+  ConverseShoe5,
+  ConverseShoe6,
+  ConverseShoe7,
+  ConverseShoe8,
+  ConverseShoe9,
+  ConverseShoe10,
+  ConverseShoe11,
+  ConverseShoe12,
+  ConverseShoe13,
+  ConverseShoe14,
+  ConverseShoe15,
+  ConverseShoe16,
+  ConverseShoe17,
+  ConverseShoe18,
+  ConverseShoe19,
+  ConverseShoe20,
+  ConverseShoe21,
+  ConverseShoe22,
+  ConverseShoe23,
+  ConverseShoe24,
+  Nike,
+  NikeShoe1,
+  NikeShoe2,
+  NikeShoe3,
+  NikeShoe4,
+  NikeShoe5,
+  NikeShoe6,
+  NikeShoe7,
+  NikeShoe8,
+  NikeShoe9,
+  NikeShoe10,
+  NikeShoe11,
+  NikeShoe12,
+  NikeShoe13,
+  NikeShoe14,
+  NikeShoe15,
+  NikeShoe16,
+  NikeShoe17,
+  NikeShoe18,
+} from "../assets/footware";
+
+const testimonials = [
+  {
+    name: "Aditi",
+    photo: { aditi },
+    review:
+      "I've been shopping for shoes online for years, and BrandWalk is by far the best experience I've had. The selection is amazing, the prices are great, and the shipping is fast. I would definitely recommend BrandWalk to anyone looking for a new pair of shoes.",
+  },
+  {
+    name: "Sahil",
+    photo: { sahil },
+    review:
+      "I'm a doctor, and I'm on my feet all day long. BrandWalk has the best selection of comfortable shoes for men, and I always find what I'm looking for. I would definitely recommend BrandWalk to any other professionals who need comfortable shoes.",
+  },
+  {
+    name: "Rohan",
+    photo: { rohan },
+    review:
+      "I've been shopping for shoes online for years, and BrandWalk is by far the best experience I've had. The selection is amazing, the prices are great, and the shipping is fast. I would definitely recommend BrandWalk to anyone looking for a new pair of shoes.",
+  },
+  {
+    name: "Prianka",
+    photo: { priyanka },
+    review:
+      "I'm a fashion blogger, and I'm always looking for new shoes to style my outfits. BrandWalk has the best selection of trendy shoes for women, and I always find what I'm looking for. I would definitely recommend BrandWalk to any other fashion-forward women.",
+  },
+];
+
+const shoppingCards = [
+  {
+    image: [
+      { AdidasShoe1 },
+      { AdidasShoe2 },
+      { AdidasShoe3 },
+      { AdidasShoe4 },
+      { AdidasShoe5 },
+      { AdidasShoe6 },
+      { AdidasShoe7 },
+      { AdidasShoe8 },
+      { AdidasShoe9 },
+      { AdidasShoe10 },
+      { AdidasShoe11 },
+    ],
+    title: "Adidas",
+  },
+];
+
+export { testimonials, shoppingCards };
