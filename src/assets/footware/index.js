5398eb06aad6955d06cdb9df5f6daa8e1463246b
Grenish rai <ariestheracer@gmail.com>
2023-08-12 12:21:48 +0530

    import/export shoe

---
+++ src/assets/footware/index.js
@@ -0,0 +1,113 @@
+import AdidasShoe1 from "./Adidas/AdidasShoe1.jpg";
+import AdidasShoe2 from "./Adidas/AdidasShoe2.jpg";
+import AdidasShoe3 from "./Adidas/AdidasShoe3.jpg";
+import AdidasShoe4 from "./Adidas/AdidasShoe4.jpg";
+import AdidasShoe5 from "./Adidas/AdidasShoe5.jpg";
+import AdidasShoe6 from "./Adidas/AdidasShoe6.jpg";
+import AdidasShoe7 from "./Adidas/AdidasShoe7.jpg";
+import AdidasShoe8 from "./Adidas/AdidasShoe8.jpg";
+import AdidasShoe9 from "./Adidas/AdidasShoe9.jpg";
+import AdidasShoe10 from "./Adidas/AdidasShoe10.jpg";
+import AdidasShoe11 from "./Adidas/AdidasShoe11.jpg";
+
+import ConverseShoe1 from "./Converse/ConverseShoe1.jpg";
+import ConverseShoe2 from "./Converse/ConverseShoe2.jpg";
+import ConverseShoe3 from "./Converse/ConverseShoe3.jpg";
+import ConverseShoe4 from "./Converse/ConverseShoe4.jpg";
+import ConverseShoe5 from "./Converse/ConverseShoe5.jpg";
+import ConverseShoe6 from "./Converse/ConverseShoe6.jpg";
+import ConverseShoe7 from "./Converse/ConverseShoe7.jpg";
+import ConverseShoe8 from "./Converse/ConverseShoe8.jpg";
+import ConverseShoe9 from "./Converse/ConverseShoe9.jpg";
+import ConverseShoe10 from "./Converse/ConverseShoe10.jpg";
+import ConverseShoe11 from "./Converse/ConverseShoe11.jpg";
+import ConverseShoe12 from "./Converse/ConverseShoe12.jpg";
+import ConverseShoe13 from "./Converse/ConverseShoe13.jpg";
+import ConverseShoe14 from "./Converse/ConverseShoe14.jpg";
+import ConverseShoe15 from "./Converse/ConverseShoe15.jpg";
+import ConverseShoe16 from "./Converse/ConverseShoe16.jpg";
+import ConverseShoe17 from "./Converse/ConverseShoe17.jpg";
+import ConverseShoe18 from "./Converse/ConverseShoe18.jpg";
+import ConverseShoe19 from "./Converse/ConverseShoe19.jpg";
+import ConverseShoe20 from "./Converse/ConverseShoe20.jpg";
+import ConverseShoe21 from "./Converse/ConverseShoe21.jpg";
+import ConverseShoe22 from "./Converse/ConverseShoe22.jpg";
+import ConverseShoe23 from "./Converse/ConverseShoe23.jpg";
+import ConverseShoe24 from "./Converse/ConverseShoe24.jpg";
+
+import Nike from "./Nike/nike.jpg";
+import NikeShoe1 from "./Nike/Nike1.jpg";
+import NikeShoe2 from "./Nike/Nike2.jpg";
+import NikeShoe3 from "./Nike/Nike3.jpg";
+import NikeShoe4 from "./Nike/Nike4.jpg";
+import NikeShoe5 from "./Nike/Nike5.jpg";
+import NikeShoe6 from "./Nike/Nike6.jpg";
+import NikeShoe7 from "./Nike/Nike7.jpg";
+import NikeShoe8 from "./Nike/Nike8.jpg";
+import NikeShoe9 from "./Nike/Nike9.jpg";
+import NikeShoe10 from "./Nike/Nike10.jpg";
+import NikeShoe11 from "./Nike/Nike11.jpg";
+import NikeShoe12 from "./Nike/Nike12.jpg";
+import NikeShoe13 from "./Nike/Nike13.jpg";
+import NikeShoe14 from "./Nike/Nike14.jpg";
+import NikeShoe15 from "./Nike/Nike15.jpg";
+import NikeShoe16 from "./Nike/Nike16.jpg";
+import NikeShoe17 from "./Nike/Nike17.jpg";
+import NikeShoe18 from "./Nike/Nike18.jpg";
+
+export {
+  AdidasShoe1,
+  AdidasShoe2,
+  AdidasShoe3,
+  AdidasShoe4,
+  AdidasShoe5,
+  AdidasShoe6,
+  AdidasShoe7,
+  AdidasShoe8,
+  AdidasShoe9,
+  AdidasShoe10,
+  AdidasShoe11,
+  ConverseShoe1,
+  ConverseShoe2,
+  ConverseShoe3,
+  ConverseShoe4,
+  ConverseShoe5,
+  ConverseShoe6,
+  ConverseShoe7,
+  ConverseShoe8,
+  ConverseShoe9,
+  ConverseShoe10,
+  ConverseShoe11,
+  ConverseShoe12,
+  ConverseShoe13,
+  ConverseShoe14,
+  ConverseShoe15,
+  ConverseShoe16,
+  ConverseShoe17,
+  ConverseShoe18,
+  ConverseShoe19,
+  ConverseShoe20,
+  ConverseShoe21,
+  ConverseShoe22,
+  ConverseShoe23,
+  ConverseShoe24,
+  Nike,
+  NikeShoe1,
+  NikeShoe2,
+  NikeShoe3,
+  NikeShoe4,
+  NikeShoe5,
+  NikeShoe6,
+  NikeShoe7,
+  NikeShoe8,
+  NikeShoe9,
+  NikeShoe10,
+  NikeShoe11,
+  NikeShoe12,
+  NikeShoe13,
+  NikeShoe14,
+  NikeShoe15,
+  NikeShoe16,
+  NikeShoe17,
+  NikeShoe18,
+};
