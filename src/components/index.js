ebb072fc51b198ab3f72a5cfdd5d3d56e627e87f
Grenish rai <ariestheracer@gmail.com>
2023-12-01 01:41:32 +0530

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Footer from "./Footer";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import ProductPage from "./ProductPage";
+import Shoes from "./Shoes";
+
+export { Footer, Home, Navbar, ProductPage, Shoes };
