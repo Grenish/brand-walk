d29a8f18b9150e93d6deefdc2423698635a18201
Grenish rai <ariestheracer@gmail.com>
2023-12-01 01:41:16 +0530

    import/export of assets

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import Logo from "./brandwalk.svg";
+import Shoe from "./shoe.png";
+import noise from "./noise.svg";
+import savings from "./savings.png";
+
+
+export { Logo, Shoe, noise, savings };
+
